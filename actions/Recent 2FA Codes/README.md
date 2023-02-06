c07311d6a0962b18e64b95db7638276efa0341ff
Manfred <git@linzner.net>
2023-02-06 12:51:01 +0000

    Add Recent 2FA Codes Action

---
+++ actions/Recent 2FA Codes/README.md
@@ -0,0 +1,18 @@
+
+
+# Recent 2FA Codes
+
+Action to quickly grab 2FA codes received via iMessages app.
+The action retrieves the 5 most recent messages that include a code between 4 and 8 digits long. It only looks at incoming SMS messages of the last 24 hours.
+
+Codes are returned as text, so all standard text features of LaunchBar will work:
+* ⌘C to copy the code
+* ⇧⌘C to copy and paste the code in one go
+* ⇧↩ to insert the code in the front-most window
+* ⇥ to send it to a different app or item inside LaunchBar.
+
+[Download Action](https://github.com/mlinzner/LaunchBarActions/blob/master/actions/Recent%202FA%20Codes.lbaction?raw=true)
+
+**Dependencies & Requirements:**
+- Make sure you are receiving SMS message in iMessage. You probably need to have an iPhone next by and SMS forwarding turned on.
+- LaunchBar needs to have Full Disk Access, else it can not read the iMessage database.
---
+++ actions/Recent 2FA Codes/Recent 2FA Codes.lbaction/Contents/Scripts/default.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+#
+# LaunchBar Action Script to Search for 2FA Codes in iMessage
+# Note: LaunchBar needs Full-Disk Access to run this action, else the database can not be opened
+import sqlite3
+import json
+import re
+import os
+
+
+def first_code_match(text):
+    # Only codes that are at the start of a message
+    # Have a whitespace before the code
+    # Have a hash infront (https://github.com/wicg/sms-one-time-codes)
+    r = re.search("^\d{4,8}|(?<=[\s#])[0-9]{4,8}", text)
+                #  ^ at the start of the message 4-8 character code
+                #                 ^ code in the message but preceted by whitespace or #
+                #                   (Positive lookbehind to ensure we do not include
+                #                    the whitespace or hash in the result)
+    return r.group(0) if r else None
+
+items = []
+
+connection = sqlite3.connect(os.path.expanduser("~/Library/Messages/chat.db"))
+cursor = connection.cursor()
+
+# Queries for message content, sender name (or number as fallback) and message date
+# * Only incoming messages (not sent by "me")
+# * In the last 5 days
+# * Received as SMS (not iMessage, iChat, …)
+# * that match multiple GLOBs (unfortunately Regex doesn't seem to be supported by sqlite on macOS Ventura.)
+query = """
+SELECT
+    message.text,
+    ifnull(handle.uncanonicalized_id, chat.chat_identifier) AS sender,
+    datetime(message.date / 1000000000 + strftime ("%s", "2001-01-01"), "unixepoch", "localtime") AS message_date
+FROM
+    chat
+    JOIN chat_message_join ON chat. "ROWID" = chat_message_join.chat_id
+    JOIN message ON chat_message_join.message_id = message. "ROWID"
+    JOIN handle ON message.handle_id = handle."ROWID"
+WHERE message.text is not null
+    AND message.is_from_me=0
+    AND datetime(message.date / 1000000000 + strftime ("%s", "2001-01-01"), "unixepoch", "localtime") > datetime ("now", "-1 days", "localtime")
+    AND message.service = "SMS"
+    AND (message.text GLOB "*[0-9][0-9][0-9][0-9]*"
+        OR message.text GLOB "*[0-9][0-9][0-9][0-9][0-9]*"
+        OR message.text GLOB "*[0-9][0-9][0-9][0-9][0-9][0-9]*"
+        OR message.text GLOB "*[0-9][0-9][0-9][0-9][0-9][0-9][0-9]*"
+        OR message.text GLOB "*[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]*"
+        )
+ORDER BY
+    message_date DESC
+LIMIT 5
+"""
+
+
+for row in cursor.execute(query):
+    code = first_code_match(row[0])
+    if code:
+        item = {}
+        item['title'] = code
+        item['subtitle'] = row[2]
+        item['badge'] = row[1]
+        item['icon'] = 'font-awesome:fa-mobile'
+        items.append(item)
+
+print (json.dumps(items))
