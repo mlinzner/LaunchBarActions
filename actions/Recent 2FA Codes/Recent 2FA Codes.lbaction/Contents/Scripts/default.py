0e55d1c56e2117e8a088e89567ede4bd81775228
Manfred <git@linzner.net>
2023-02-07 08:40:23 +0000

    Update comments

--- actions/Recent 2FA Codes/Recent 2FA Codes.lbaction/Contents/Scripts/default.py
+++ actions/Recent 2FA Codes/Recent 2FA Codes.lbaction/Contents/Scripts/default.py
@@ -12,23 +12,28 @@ def first_code_match(text):
     # Only codes that are at the start of a message
     # Have a whitespace before the code
     # Have a hash infront (https://github.com/wicg/sms-one-time-codes)
+
+    # Because of the limited GLOB syntax in the database query, we will
+    # also receive things like messages with a shared phone number
+    # that should be filtered out here.
     r = re.search("^\d{4,8}|(?<=[\s#])[0-9]{4,8}", text)
                 #  ^ at the start of the message 4-8 character code
-                #                 ^ code in the message but preceted by whitespace or #
+                #                 ^ code within the message text with whitespace or # in front
                 #                   (Positive lookbehind to ensure we do not include
                 #                    the whitespace or hash in the result)
     return r.group(0) if r else None
 
 items = []
-
 connection = sqlite3.connect(os.path.expanduser("~/Library/Messages/chat.db"))
 cursor = connection.cursor()
 
 # Queries for message content, sender name (or number as fallback) and message date
 # * Only incoming messages (not sent by "me")
-# * In the last 5 days
+# * Received in the last 24 hours
 # * Received as SMS (not iMessage, iChat, …)
-# * that match multiple GLOBs (unfortunately Regex doesn't seem to be supported by sqlite on macOS Ventura.)
+# * Message content that matches one of the GLOBs
+#   (unfortunately Regex doesn't seem to be supported by sqlite on macOS Ventura.)
+# * The 5 most recent items
 query = """
 SELECT
     message.text,
